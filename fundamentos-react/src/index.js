42ce6321f5a6241c6b43cfd8a687e6e696a71cb2
Renan Paiva <renanvsp@hotmail.com>
2022-08-01 14:07:45 -0300

    componentes com propriedades

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,11 +3,15 @@ import React from 'react'
 import './index.css'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDom.render(
-    <div>
-        <Primeiro></Primeiro>   
+    <div id="app">
+        <Primeiro></Primeiro> 
+        <ComParametro 
+            titulo="situação do aluno" 
+            aluno="Renan Paiva" 
+            nota={ 9.3 }/>
     </div>,
     document.getElementById('root')
      )
-

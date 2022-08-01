04207540c932819e65eb6cb360d6b706856eb443
Renan Paiva <renanvsp@hotmail.com>
2022-08-01 11:15:22 -0300

    first commit

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,13 @@
+import ReactDom from 'react-dom'
+import React from 'react'
+import './index.css'
+
+import Primeiro from './components/basicos/Primeiro'
+
+ReactDom.render(
+    <div>
+        <Primeiro></Primeiro>   
+    </div>,
+    document.getElementById('root')
+     )
+

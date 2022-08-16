302f55278d7875b8aae3e1b0865553d9219272e7
Renan Paiva <renanvsp@hotmail.com>
2022-08-16 11:34:42 -0300

    aula 21 finalizada

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 
+import Card from "./components/layout/Card"
 import Aleatorio from "./components/Aleatorio";
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 
 export default () => (
-        <div id="app">
-            <h1>Fundamentos React</h1>
+    <div id="app">
+        <h1>Fundamentos React</h1>
+
+        <Card titulo="#04 - Desafio aleatório">
             <Aleatorio min={10} max={60} />
+        </Card>
+
+        <Card titulo="#03- Fragmento">
             <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parâmetro">
             <ComParametro
                 titulo="situação do aluno"
                 aluno="Renan Paiva"
                 nota={9.3}
             />
+        </Card>
+
+        <Card titulo="#01 - Primeiro Componente">
             <Primeiro></Primeiro>
-        </div>
+        </Card>
+    </div>
 );
